e7f5152ab716b921dc13fe8a2a2d8a28b436f80b
TranQuocVietUIT <79837616+TranQuocVietUIT@users.noreply.github.com>
2021-03-31 16:06:28 +0700

    modify in 16:02 31/3
    modify in 16:02 31/3

--- Additional_File/test1.py
+++
@@ -1,3 +0,0 @@
-import shutil
-
-shutil.copyfile('/home/viet/Documents/UIT/CE434.L21-Group3/Assignment_2/Output/Bin_In/bin_in_0.txt', '/home/viet/Documents/UIT/CE434.L21-Group3/Additional_File/file2.txt')
