f51c64705d62a8eceebf0dd172fccc63638320a5
TranQuocVietUIT <79837616+TranQuocVietUIT@users.noreply.github.com>
2021-04-02 11:23:55 +0700

    modify in 2/4 11:23

--- Assignment_2/main.py
+++ Assignment_2/main.py
@@ -5,8 +5,8 @@ import numpy as np
 
 os.chdir('/home/viet/Documents/UIT/CE434.L21-Group3/Assignment_2/Output/Image_Before_Processing')
 num_image = len([name for name in os.listdir('.') if os.path.isfile(name)])
-# os.system('python3 /home/viet/Documents/UIT/CE434.L21-Group3/Assignment_2/Python_Script/Convert_Video_To_Image.py')
-# os.system('python3 /home/viet/Documents/UIT/CE434.L21-Group3/Assignment_2/Python_Script/RGB2Binary.py')
+os.system('python3 /home/viet/Documents/UIT/CE434.L21-Group3/Assignment_2/Python_Script/Convert_Video_To_Image.py')
+os.system('python3 /home/viet/Documents/UIT/CE434.L21-Group3/Assignment_2/Python_Script/RGB2Binary.py')
 for i in range (num_image):
 	shutil.copyfile('/home/viet/Documents/UIT/CE434.L21-Group3/Assignment_2/Output/Binary_Files_Before_Processing/bin_in_'+str(i)+'.txt', '/home/viet/Documents/UIT/CE434.L21-Group3/Assignment_2/Output/bin_in_temp.txt')
 	os.system('vlog /home/viet/Documents/UIT/CE434.L21-Group3/Assignment_2/RTL/rgb2grayscale.v /home/viet/Documents/UIT/CE434.L21-Group3/Assignment_2/TestBench/rgb2grayscale_tb.v')
